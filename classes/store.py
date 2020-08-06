e27bc8d2aaea6847f73a43df3617382081c7a05e
Pelegrinetti <pelegrinetti.dev@gmail.com>
2020-08-06 19:54:32 -0300

    add store class

---
+++ classes/store.py
@@ -0,0 +1,31 @@
+import os
+import time
+import json
+
+
+class Store:
+    def __init__(self, store_folder):
+        self.store_folder = store_folder
+
+        os.makedirs(store_folder, exist_ok=True)
+        self.file_path = os.path.join(store_folder, "data.json")
+
+        if not os.path.exists(self.file_path):
+            print('> Creating data file...')
+            with open(self.file_path, 'w') as file:
+                json.dump({'metrics': []}, file)
+                file.close()
+            print('> Data file created!')
+
+    def store(self, values):
+        with open(self.file_path, 'r+') as file:
+            data = json.loads(file.read())
+            data['metrics'].append(values)
+            file.seek(0)
+            file.write(json.dumps(data))
+            file.truncate()
+
+    def get(self):
+        with open(self.file_path, 'r') as file:
+            data = json.loads(file.read())
+            return data['metrics']
